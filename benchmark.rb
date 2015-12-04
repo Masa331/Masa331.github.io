722ab1952931889221cdc6fc5f0f80c5fed4e84d
Premysl Donat <pdonat@seznam.cz>
2015-12-04 21:06:42 +0100

    fix article about string benchmark

--- benchmark.rb
+++ benchmark.rb
@@ -2,6 +2,6 @@ require 'benchmark'
 
 Benchmark.bmbm do |x|
    x.report { 10000.times { 'donat@gmail.com'.tr('@.', '') } }
-   x.report { 10000.times { 'donat@gmail.com'.delete('@').delete('.') } }
+   x.report { 10000.times { 'donat@gmail.com'.delete('@.') } }
    x.report { 10000.times { 'donat@gmail.com'.gsub('@', '').gsub('.', '') } }
 end
