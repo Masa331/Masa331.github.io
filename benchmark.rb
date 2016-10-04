105fc30540069283d4142b97eb96e8d0000b64b3
Premysl Donat <pdonat@seznam.cz>
2016-10-04 23:00:52 +0200

    Remove unnecessarry files

--- benchmark.rb
+++
@@ -1,7 +0,0 @@
-require 'benchmark'
-
-Benchmark.bmbm do |x|
-   x.report { 10000.times { 'donat@gmail.com'.tr('@.', '') } }
-   x.report { 10000.times { 'donat@gmail.com'.delete('@.') } }
-   x.report { 10000.times { 'donat@gmail.com'.gsub('@', '').gsub('.', '') } }
-end
